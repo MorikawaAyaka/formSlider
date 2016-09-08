dea77ff44de5bb176bed0cecd33606d1cb8e9ac1
森川綾香 <AyakaMorikawa@kenshuuyoupasokon-no-MacBook-Air.local>
2016-09-08 16:56:10 +0900

    未回答のものの背景の色を変更

--- form.js
+++ form.js
@@ -3,67 +3,83 @@
   var slideNum = $('.slide').length; //slideの数
   var radioNum = 9;
 
-  for(var i = 2;i <= slideNum;i++){
-    $('#d'+i).css('display','none');
+  for (var i = 2; i <= slideNum; i++) {
+    $('#d' + i).css('display', 'none');
   }
-  $('.submit').css('display','none');
-  $('.buttonPrevious').css('display','none');
+  $('.submit').css('display', 'none');
+  $('.buttonPrevious').css('display', 'none');
 
-  var validate = function(){
-    var message = 'Please select an option';
-
-  }
-
-  // 前へボタンが押されたとき
+  var validation = function() {
+      for (var i = 1; i <= radioNum; i++) {
+        var check = $("#q" + cnt + i).find('input[type="radio"]:checked').size();
+        if (check == 0) {
+          $('#q' + cnt + i).css("background-color", "#FFB6C1");
+        } else {
+          $('#q' + cnt + i).css("background-color", "#FFFFFF");
+        }
+      }
+    }
+    // 前へボタンが押されたとき
   $('.buttonPrevious').click(function() {
-    $('#d'+cnt).toggle(false);
-    $('#d'+(cnt-1)).toggle(true);
+    $('#d' + cnt).toggle(false);
+    $('#d' + (cnt-1)).toggle(true);
     cnt--;
 
-    if(cnt == slideNum){
+    if (cnt == slideNum) {
       $('.buttonNext').toggle(false);
       $('.submit').toggle(true);
-    }else{
+    } else {
       $('.buttonNext').toggle(true);
       $('.submit').toggle(false);
     }
 
-    if(cnt == 1){
+    if (cnt == 1) {
       $('.buttonPrevious').toggle(false);
-    }else{
+    } else {
+      $('.buttonPrevious').toggle(true);
+    }
+    validation();
+  });
+
+  // 次へボタンが押されたとき
+  $('.buttonNext').click(function() {
+    //チェックされているラジオボタンの数を数える
+    var checked_radio_count = $(".group" + cnt).find('input[type="radio"]:checked').size();
+    //ラジオボタンが入っているdivの数を数える
+    var radio_count = $(".group" + cnt).size();
+
+    if (checked_radio_count == radio_count) {
+      $('#d' + cnt).toggle(false);
+      $('#d' + (cnt + 1)).toggle(true);
+      cnt++;
+    } else {
+      validation();
+    }
+    if (cnt == slideNum) {
+      $('.buttonNext').toggle(false);
+      $('.submit').toggle(true);
+    } else {
+      $('.buttonNext').toggle(true);
+      $('.submit').toggle(false);
+    }
+    if (cnt == 1) {
+      $('.buttonPrevious').toggle(false);
+    } else {
       $('.buttonPrevious').toggle(true);
     }
 
   });
 
-  // 次へボタンが押されたとき
-  $('.buttonNext').click(function(){
-    //チェックされているラジオボタンの数を数える
-   var checked_radio_count = $(".group"+cnt).find('input[type="radio"]:checked').size();
-   //ラジオボタンが入っているdivの数を数える
-   var radio_count = $(".group"+cnt).size();
+  $('.submit').click(function(){
+    var last_radio_count = $(".group" + slideNum).find('input[type="radio"]:checked').size();
+    var last_radio = $(".group" + slideNum).size();
 
-   if(checked_radio_count == radio_count){
-     $('#d'+cnt).toggle(false);
-     $('#d'+(cnt+1)).toggle(true);
-      cnt++;
-  }else{
-     $('#q1').css("background-color","#FFB6C1");
-
-}
-  if(cnt == slideNum){
-    $('.buttonNext').toggle(false);
-    $('.submit').toggle(true);
-  }else{
-    $('.buttonNext').toggle(true);
-    $('.submit').toggle(false);
-  }
-  if(cnt == 1){
-    $('.buttonPrevious').toggle(false);
-  }else{
-    $('.buttonPrevious').toggle(true);
-  }
-
+    if(last_radio_count == last_radio){
+      $(this).submit();
+    }else{
+      $(this).off('submit');
+      validation();
+    }
   });
 
 }());
